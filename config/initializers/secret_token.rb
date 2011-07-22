8c345540b4a922c3d05858eecabe3e991a9561c0
Duncan Gough <duncan@suttree.com>
2011-07-22 21:40:04 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magix8box::Application.config.secret_token = '31f85b271db578eaca3f139285542b267cfdb30028a8f65122e148559c630efd93dab5032c935338d5d9888781b3b6759e2043647bd24b63541a78f9f70ea323'
